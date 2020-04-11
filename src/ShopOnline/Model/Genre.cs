e87fe242019288995a49746b64ad5678a03e1a71
AgnieszkaChS <a.chruszczyk@gmail.com>
2020-04-11 21:02:45 +0200

    Remove unused properites in Genre Class

--- src/ShopOnline/Model/Genre.cs
+++ src/ShopOnline/Model/Genre.cs
@@ -3,9 +3,6 @@ namespace ShopOnline.Model
 {
     public class Genre
     {
-        private int v1;
-        private string v2;
-
         public int Id { get; set; }
         public string Name { get; set; }
 
