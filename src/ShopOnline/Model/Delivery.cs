6efa9fcd50742d9a515d54ae0f472d5ea7a1c367
AgnieszkaChS <a.chruszczyk@gmail.com>
2020-04-12 18:13:35 +0200

    Create Payment and Delivery classes

---
+++ src/ShopOnline/Model/Delivery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ShopOnline.Model
+{
+    public class Delivery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+
+        public Delivery()
+        {
+        }
+
+        public Delivery(int id, string name, double cost)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+        }
+    }
+}
---
+++ src/ShopOnline/Model/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ShopOnline.Model
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+
+        public Payment()
+        {
+        }
+
+        public Payment(int id, string name, double cost)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+        }
+    }
+}
