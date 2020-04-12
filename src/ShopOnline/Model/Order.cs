02a0264ca5e4666ba4b9f74e46676011754d5ab1
AgnieszkaChS <a.chruszczyk@gmail.com>
2020-04-12 18:13:49 +0200

    Create Order class

--- src/ShopOnline/Model/Order.cs
+++ src/ShopOnline/Model/Order.cs
@@ -3,8 +3,28 @@ namespace ShopOnline.Model
 {
     public class Order
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Cart Cart { get; set; }
+        public Payment Payment { get; set; }
+        public Delivery Delivery { get; set; }
+        public double TotalPrice { get; set; }
+        public Customer Customer { get; set; }
+
         public Order()
         {
         }
+
+        public Order(int id, DateTime date, Cart cart, Payment payment, Delivery delivery, double totalPrice, Customer customer)
+        {
+            Id = id;
+            Date = date;
+            Cart = cart;
+            Payment = payment;
+            Delivery = delivery;
+            TotalPrice = totalPrice;
+            Customer = customer;
+        }
+
     }
 }
