930b4ce1478136b62d3d614866bc3710839f829a
AgnieszkaChS <a.chruszczyk@gmail.com>
2020-04-13 23:32:37 +0200

    Add MainController and CartItemDao

---
+++ src/ShopOnline/DataAccess/ICartItemDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using ShopOnline.Model;
+
+namespace ShopOnline.DataAccess
+{
+    public interface ICartItemDao
+    {
+        void AddToBasket(int product_id, int cart_id);
+        void ChangeQuantity(int product_id, int quantity);
+        List<CartItem> GetCardItem();
+        void RemoveWhenIsZero();
+        void ClearCart();
+    }
+}
