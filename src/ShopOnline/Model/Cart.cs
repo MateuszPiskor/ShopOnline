9e222ca5e3747d6a1252717d237caf20cfcbc3da
AgnieszkaChS <a.chruszczyk@gmail.com>
2020-04-13 16:45:52 +0200

    Add properties to Cart class

--- src/ShopOnline/Model/Cart.cs
+++ src/ShopOnline/Model/Cart.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ShopOnline.Model
 {
     public class Cart
     {
+        public int Id { get; set; }
+        public double  TotalPrice { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
         public Cart()
         {
         }
