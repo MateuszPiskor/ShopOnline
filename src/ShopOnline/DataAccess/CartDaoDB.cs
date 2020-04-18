7fef2f30a01953c3a341c23635dcd74636cf0196
Magda <magda.opiola@gmail.com>
2020-04-18 12:04:14 +0200

    Switched db connection

--- src/ShopOnline/DataAccess/CartDaoDB.cs
+++ src/ShopOnline/DataAccess/CartDaoDB.cs
@@ -12,9 +12,9 @@ namespace ShopOnline.DataAccess
     
         public CartDaoDB()
         {
-            DataBaseConnectionService = new DataBaseConnectionService("localhost", "magda", "Lena1234", "onlineshop");
+            //DataBaseConnectionService = new DataBaseConnectionService("localhost", "magda", "Lena1234", "onlineshop");
             //DataBaseConnectionService = new DataBaseConnectionService("localhost", "postgres", "1234", "ShopOnline");
-            //DataBaseConnectionService = new DataBaseConnectionService("localhost", "agnieszkachruszczyksilva", "startthis", "shop_online_project");
+            DataBaseConnectionService = new DataBaseConnectionService("localhost", "agnieszkachruszczyksilva", "startthis", "shop_online_project");
         }
 
 
